9a0732720b4c8b935dad7cddbf8f1874a0295a53
lmanaya <lmanaya990320@gmail.com>
2020-12-18 11:21:38 -0500

    se modificó el usuarioController

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -5,26 +5,7 @@ const bcrypt = require('bcryptjs');
 
 const tokenServices = require('../services/token.js');
 
-//Métodos
-exports.listar = async (req, res, next) => {
-    try {
-        const user = await models.Usuario.findAll();
-        if (user) {
-            res.status(200).json(user);
-        }
-        else {
-            res.status(404).send({
-                message: 'There is not user in the system'
-            })
-        }
-    }
-    catch (error) {
-        res.status(500).send({
-            message: 'Error ->'
-        });
-        next(error);
-    }
-};
+//Login
 exports.login = async (req, res, next) => {
     try {
         // Verificar que el usuario exista
@@ -65,51 +46,5 @@ exports.login = async (req, res, next) => {
             message: 'Error'
         }),
             next(error);
-    }
-}
-exports.register = async (req, res, next) => {
-    try {
-        const user = await models.Usuario.findOne({ where: { email: req.body.email } });
-        if (user) {
-            res.status(409).send({
-                message: 'Sorry your request has a conflict with our system state, maybe the email is already'
-            });
-        }
-        else {
-            req.body.password = bcrypt.hashSync(req.body.password, 10);
-            const user = await models.Usuario.create(req.body);
-            res.status(200).json(user);
-        }
-    }
-    catch (error) {
-        res.status(500).send({
-            message: 'Error ->'
-        });
-        next(error);
-    }
-}
-exports.update = async (req, res, next) => {
-    try {
-        const user = await models.Usuario.findOne({ where: { email: req.body.email } });
-        if (user) {
-            const user = await models.Usuario.update({ nombre: req.body.nombre, rol: req.body.rol },
-                {
-                    where: {
-                        email: req.body.email
-                    }
-                });
-            res.status(200).json(user);
-        }
-        else {
-            res.status(404).send({
-                message: "User not found"
-            })
-        }
-    }
-    catch (error) {
-        res.status(500).send({
-            message: 'Error ->'
-        });
-        next(error);
     }
 }

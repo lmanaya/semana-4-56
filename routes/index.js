69527b18546a6bfc49216f58e3398522fa91f1fb
lmanaya <lmanaya990320@gmail.com>
2020-12-18 07:53:04 -0500

    Reinicio

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,3 @@
 
 // module.exports = router;
 //
-//Ruta express
-const router = require('express').Router();
-
-// Manejo de las rutas
-const apiRouterUsuario = require('./api/usuarios.js');
-const apiRouterArticulo = require('./api/articulos.js');
-const apiRouterCategoria = require('./api/categorias.js');
-
-
-router.use('/usuario', apiRouterUsuario); // .com/api/usuario/
-router.use('/articulo', apiRouterArticulo); // .com/api/articulo/
-router.use('/categoria', apiRouterCategoria); // .com/api/categoria/
-
-// Exportar el router
-module.exports = router;

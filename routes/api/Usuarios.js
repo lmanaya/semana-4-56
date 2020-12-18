d0adb925c473381f2ac32a6ed4d7c6431979f67b
lmanaya <lmanaya990320@gmail.com>
2020-12-17 21:33:01 -0500

    Commit

--- routes/api/Usuarios.js
+++ routes/api/Usuarios.js
@@ -5,9 +5,9 @@ const usuarioController = require('../../controllers/usuarioController.js');
 //Ruta del middlewaer
 const auth = require('../../middlewares/auth')
 
-router.get('/list', auth.verificarVendedor, userController.list); // listar .com/api/auth/
-router.post('/register', auth.verificarAdministrador, userController.register);// register .com/api/auth/register
-router.put('/update', auth.verificarAdministrador, userController.update); // update .com/api/auth/update
+router.get('/list', auth.verificarVendedor, usuarioController.list); // listar .com/api/auth/
+router.post('/register', auth.verificarAdministrador, usuarioController.register);// register .com/api/auth/register
+router.put('/update', auth.verificarAdministrador, usuarioController.update); // update .com/api/auth/update
 
 router.post('/login', usuarioController.login); // .com/api/usuario/login
 

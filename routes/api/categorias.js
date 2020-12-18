8b2509f55f309e9590137236805d126857f0aee7
lmanaya <lmanaya990320@gmail.com>
2020-12-18 11:20:12 -0500

    se agregaron las rutas

---
+++ routes/api/categorias.js
@@ -0,0 +1,16 @@
+//Express
+const router = require('express').Router();
+// Ruta del controlador
+const categoriaController = require('../../controllers/categoriaController.js');
+//Ruta del middlewaer
+const auth = require('../../middlewares/auth')
+
+
+router.get('/list', auth.verificarVendedor, categoriaController.list); // .com/api/categoria/list
+router.post('/add', auth.verificarVendedor, categoriaController.add); // .com/api/categoria/add
+router.put('/update', auth.verificarVendedor, categoriaController.update); // .com/api/categoria/update
+router.put('/activate', auth.verificarVendedor, categoriaController.activate); // .com/api/categoria/activate
+router.put('/deactivate', auth.verificarVendedor, categoriaController.desactivate); // .com/api/categoria/desactivate
+
+// Exportar el router
+module.exports = router;

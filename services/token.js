1c6c04a0d8bf60411bde157394f89e0a68f20f7c
lmanaya <lmanaya990320@gmail.com>
2020-12-18 11:23:05 -0500

    Se agregó el checktoken junto a las funciones encode y decode

--- services/token.js
+++ services/token.js
@@ -1,20 +1,63 @@
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const models = require('../models');
 
+const checkToken = async (token) => {
+    let localID = null;
+    try {
+        const id = token.decode(token);
+        localID = id;
+    }
+    catch (error) {
+
+    }
+    const user = await models.Usuario.findOne({
+        where: {
+            id: id,
+            estado: 1,
+        }
+    });
+    if (user) {
+        const token = this.encode(user);
+        return {
+            token,
+            rol : user.rol,
+        }
+    }
+    else {
+        return false
+    }
+};
 
 module.exports = {
-
-    //generar el token
-    encode: async(id, rol) => {
-
+    encode: (user) => {
+        const token = jwt.sign({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            rol: user.rol,
+            status : user.estado,
+        }, 'config.secret', { expiresIn: 86400 });
+        return token;
     },
-    //permite decodificar el token
-    decode: async(token) => {
+    decode: async (token) => {
         try {
-
-        } catch (e) {
-
+            const {id} = await jwt.verify(token, 'config.secret');
+            const user = await models.Usuario.findOne({
+                where: {
+                    id: id,
+                    estado: 1,
+                }
+            });
+            if (user) {
+                return user;
+            }
+            else {
+                return false;
+            }
         }
-
+        catch (error) {
+            const newToken = await checkToken(token);
+            return newToken
+        }
     }
 }

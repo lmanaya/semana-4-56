3ec689e818a83f02c1afdee8e4d1614ec1cdff32
lmanaya <lmanaya990320@gmail.com>
2020-12-18 11:13:15 -0500

    Se agregó el middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,21 @@
-//Middleware de autenticacion;
-const tokenService = require('../services/token');
-
+const tokenServices = require('../services/token.js');
 module.exports = {
-    verifyUsuario: async(req, res, next) => {
+    verificarVendedor: async (req, res, next) => {
         if (!req.headers.token) {
             return res.status(404).send({
-                message: 'No token'
-            });
+                message: 'There is not token'
+            })
         }
-        const response = await tokenService.decode(req.headers.token);
-        if (response.rol == 'Administrador' || response.rol == 'Vendedor' || response.rol == 'Almacenero') {
-            next();
-        } else {
-            return res.status(403).send({
-                message: 'No autorizado'
-            });
+        else {
+            const response = await tokenServices.decode(req.headers.token);
+            if (response.rol === 'Administrador' || response.rol === 'Vendedor') {
+                next();
+            }
+            else {
+                return res.status(403).send({
+                    message: 'Unauthorized user'
+                })
+            }
         }
-    },
-
+    }
 }
